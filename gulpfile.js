b21663fc194bd34a3eb6a349e3b3ab5b9731c6db
maxnesm <maxnesm1706@gmail.com>
2022-09-21 03:01:07 +0300

    Автоматизация сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,38 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
+import {deleteAsync} from 'del'
 import postcss from 'gulp-postcss';
+import cssMinify from "postcss-csso";
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import svgstore from 'gulp-svgstore';
+import rename from 'gulp-rename';
+import svgo from 'gulp-svgmin';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from "gulp-libsquoosh";
+
+// Svg sprite
+
+const sprite = () => {
+  return gulp.src('source/img/icons/*.svg')
+    .pipe(svgo())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img/'))
+}
+
+// Svg copy
+
+const svg = () => {
+  return gulp.src(['source/img/*.svg', "!source/img/icons/*.svg"])
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img/'))
+}
+
 
 // Styles
 
@@ -12,10 +41,67 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      cssMinify()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
+}
+
+
+// Html
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("build/"));
+}
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/*.js")
+    .pipe(terser())
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest("build/js/"));
+}
+
+// Images
+
+const optimazeImages = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(squoosh())
+    .pipe(gulp.dest("source/img/"));
+}
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(gulp.dest("build/img/"));
+}
+
+// Webp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(squoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest("build/img/"));
+}
+
+// Copy
+
+const copyFiles = (done) => {
+  return gulp.src([
+    "source/fonts/*.{woff,woff2}",
+    "source/*.ico",
+    "source/*.webmanifest",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
 }
 
 // Server
@@ -23,7 +109,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -31,6 +117,13 @@ const server = (done) => {
   });
   done();
 }
+
+// Clean
+const clean = () => {
+  return deleteAsync("build")
+}
+
+
 
 // Watcher
 
@@ -40,6 +133,35 @@ const watcher = () => {
 }
 
 
+
+export const build = gulp.series(
+  clean,
+  optimazeImages,
+  copyImages,
+  copyFiles,
+  createWebp,
+  gulp.parallel(
+    html,
+    styles,
+    sprite,
+    svg,
+    scripts
+  )
+);
+
 export default gulp.series(
-  styles, server, watcher
+  clean,
+  // optimazeImages,
+  copyImages,
+  copyFiles,
+  createWebp,
+  gulp.parallel(
+    html,
+    styles,
+    sprite,
+    svg,
+    scripts
+  ),
+  server,
+  watcher
 );
---
+++ source/js/videoplayer.js
@@ -0,0 +1,16 @@
+$(function() {
+  var videos  = $(".videoplayer");
+    videos.on("click", function(){
+        var elm = $(this),
+            conts   = elm.contents(),
+            le      = conts.length,
+            ifr     = null;
+
+        for(var i = 0; i<le; i++){
+          if(conts[i].nodeType == 8) ifr = conts[i].textContent;
+        }
+
+        elm.addClass("player").html(ifr);
+        elm.off("click");
+    });
+});

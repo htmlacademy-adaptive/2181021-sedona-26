408c81b0427801b54e88ffbf7c7589289d94dcb6
maxnesm <maxnesm1706@gmail.com>
2022-09-25 18:26:57 +0300

    Исправление ошибок(защита)

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ const html = () => {
 const scripts = () => {
   return gulp.src("source/js/*.js")
     .pipe(terser())
-    // .pipe(rename('script.min.js'))
+    .pipe(rename('script.min.js'))
     .pipe(gulp.dest("build/js/"));
 }
 
